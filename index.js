1aa0d265f00e537519fe894f81a70d0fcd2ee966
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-06-07 20:09:30 +0600

    index.js and gitignore file added

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
