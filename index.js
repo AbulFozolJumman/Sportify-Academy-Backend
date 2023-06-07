8a19a852fb37ed550d64e81b46c11696447e1cda
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-06-07 20:18:40 +0600

    Default get and scripts added at package.json

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Default Get
+app.get("/", (req, res)=> {
+    res.send("Sportify Academy Server is Running")
+})
+
+// Port
+app.listen(port, ()=> {
+    console.log(`Server is running on port: ${port}`)
+})
