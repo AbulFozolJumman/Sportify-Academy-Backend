d8cf0a2f1be1f8bc9e195e4de19fde80177db00c
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-06-10 22:29:53 +0600

    Get all classes data from mongoDb done

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// MongoDB uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ztfqf2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,11 +24,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    
-    // Collections
+    // await client.connect();
+
+    // DB Collections
     const usersCollection = client.db("Sportify-Academy").collection("users");
     const classesCollection = client.db("Sportify-Academy").collection("classes");
+
+    // Get all Classes data
+    app.get('/classes', async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -42,11 +53,11 @@ run().catch(console.dir);
 
 
 // Default Get
-app.get("/", (req, res)=> {
-    res.send("Sportify Academy Server is Running")
+app.get("/", (req, res) => {
+  res.send("Sportify Academy Server is Running")
 })
 
 // Port
-app.listen(port, ()=> {
-    console.log(`Server is running on port: ${port}`)
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
 })
